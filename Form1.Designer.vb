0e2293863537552770ee7ccb4425bb3a87cc0f89
Nikos Kechris <74186791+nkechris@users.noreply.github.com>
2020-12-15 01:01:01 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtFilePath = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.txtHash = New System.Windows.Forms.TextBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(116, 9)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(142, 25)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "NK Antivirus"
+        '
+        'txtFilePath
+        '
+        Me.txtFilePath.Location = New System.Drawing.Point(12, 49)
+        Me.txtFilePath.Name = "txtFilePath"
+        Me.txtFilePath.Size = New System.Drawing.Size(246, 20)
+        Me.txtFilePath.TabIndex = 2
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(264, 49)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(81, 21)
+        Me.btnBrowse.TabIndex = 3
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtHash
+        '
+        Me.txtHash.Location = New System.Drawing.Point(12, 76)
+        Me.txtHash.Name = "txtHash"
+        Me.txtHash.Size = New System.Drawing.Size(246, 20)
+        Me.txtHash.TabIndex = 4
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.Location = New System.Drawing.Point(12, 104)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(43, 13)
+        Me.lblStatus.TabIndex = 6
+        Me.lblStatus.Text = "Status"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(61, 104)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblResult.TabIndex = 7
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(287, 83)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "MD5"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(369, 144)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.txtHash)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtFilePath)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "NK Antivirus"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtHash As TextBox
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+Public Class Form1
+
+    Function md5_hash(ByVal file_name As String)
+        Return hash_generator("md5", file_name)
+    End Function
+
+    Function hash_generator(ByRef hash_type As String, ByRef file_name As String)
+
+        Dim hash
+        hash = MD5.Create
+
+        Dim hashValue() As Byte
+
+        Dim filestream As FileStream = File.OpenRead(file_name)
+        filestream.Position = 0
+        hashValue = hash.ComputeHash(filestream)
+        Dim hash_hex = PrintByteArray(hashValue)
+
+        filestream.Close()
+
+        Return hash_hex
+    End Function
+
+    Public Function PrintByteArray(ByRef array() As Byte)
+
+        Dim hex_value As String = ""
+
+        Dim i As Integer
+        For i = 0 To array.Length - 1
+
+            hex_value += array(i).ToString("x2")
+        Next i
+
+        Return hex_value.ToLower
+    End Function
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim path As String = OpenFileDialog1.FileName
+            txtFilePath.Text = path
+
+            Dim sample As String
+            sample = md5_hash(path)
+            txtHash.Text = md5_hash(path)
+
+            Using f As System.IO.FileStream = System.IO.File.OpenRead("md5.txt")
+                Using s As System.IO.StreamReader = New System.IO.StreamReader(f)
+                    While Not s.EndOfStream
+                        Dim line As String = s.ReadLine
+
+                        If (line = sample) Then
+                            lblResult.Text = "Infected!"
+                            lblResult.ForeColor = Color.Red
+                        Else
+                            lblResult.Text = "Clean!"
+                            lblResult.ForeColor = Color.Green
+                        End If
+                    End While
+                End Using
+            End Using
+        End If
+    End Sub
+
+    Private Sub txtFilePath_TextChanged(sender As Object, e As EventArgs) Handles txtFilePath.TextChanged
+
+    End Sub
+
+    Private Sub lblSig_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+
+    Private Sub lblTitle_Click(sender As Object, e As EventArgs) Handles lblTitle.Click
+
+    End Sub
+
+    Private Sub lblMD5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub txtHash_TextChanged(sender As Object, e As EventArgs) Handles txtHash.TextChanged
+
+    End Sub
+End Class
